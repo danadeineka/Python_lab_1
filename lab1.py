fec11ca85c39008d5464eba432e803c2e8457cf5
danadeineka <danadeyneka9@gmail.com>
2021-02-24 21:43:41 +0200

    edited lab1.py file

--- lab1.py
+++ lab1.py
@@ -1,13 +1,14 @@
 class InvestmentCompany:
-    data_source='PitchBook Company'
+    data_source = 'PitchBook Company'
 
-    def __init__(self, country="", employees=0, name="", address="", capital=0, investments=0,):
-        self.country=country
-        self.employees=employees
-        self.name=name
-        self.address=address
-        self.capital=capital
-        self.investments=investments
+    def __init__(self, country="", employees=0, name="",
+                 address="", capital=0, investments=0,):
+        self.country = country
+        self.employees = employees
+        self.name = name
+        self.address = address
+        self.capital = capital
+        self.investments = investments
 
     def __del__(self):
         pass
@@ -20,18 +21,28 @@ class InvestmentCompany:
                f"Capital: {self.capital}\n" \
                f"Investments: {self.investments}"
 
-
-
     @staticmethod
     def show_data_source():
         return InvestmentCompany.data_source
 
-def main():
-        company1 = InvestmentCompany("USA", 75, "Accel-KKR", " 2500 Sand Hill Road Suite 300. Menlo Park, California 94025", 10, 200)
-        company2 = InvestmentCompany("USA", 180, "Berkshire Partners","200 Clarendon Street 35th Floor Boston, MA 02116 USA", 16, 100)
-        company3 = InvestmentCompany("USA", 41, "Brentwood Associates","11150 Santa Monica Blvd.Suite 1200Los Angeles, CA 90025", 3, 50)
 
-        print(company1, company2, company3)
+def main():
+    company1 = InvestmentCompany(
+        "USA", 75, "Accel-KKR",
+        " 2500 Sand Hill Road Suite 300. Menlo Park, California 94025",
+        10, 200
+    )
+    company2 = InvestmentCompany("USA", 180, "Berkshire Partners",
+                                 f"200 Clarendon Street 35th "
+                                 f"Floor Boston, MA 02116 USA",
+                                 16, 100)
+    company3 = InvestmentCompany("USA", 41, "Brentwood Associates",
+                                 f"11150 Santa Monica Blvd.Suite"
+                                 f"1200Los Angeles, CA 90025",
+                                 3, 50)
+
+    print(company1, company2, company3)
+
 
 if __name__ == "__main__":
-        main()
+    main()
